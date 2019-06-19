da04a3cd2a1d6cb2991e174c40c2c1c55a3e2378
dextraz <dextrazdxt@yahoo.com>
2019-06-19 21:07:26 +0300

    Capitalise Sentence

---
+++ CapitaliseSentence/capitalize-sentence.js
@@ -0,0 +1,24 @@
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
+// Version 2
+
+function capitalize(str) {
+    const words = [];
+
+    for (let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+
+    return words.join(' ');
+}
