60a8a0859a2550180e8463f35190995fc91d649b
dextraz <dextrazdxt@gmail.com>
2019-08-08 17:53:52 +0300

    Weaving the Queues

---
+++ WeavingQueues/queueWeaving.js
@@ -0,0 +1,31 @@
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
+
+function weave(sourceOne, sourceTwo) {
+  const q = new Queue();
+
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) {
+      q.add(sourceOne.remove());
+    }
+
+    if (sourceTwo.peek()) {
+      q.add(sourceTwo.remove());
+    }
+  }
+
+  return q;
+}
+
+module.exports = weave;
