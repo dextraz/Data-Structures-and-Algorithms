fe25fee1050c222f4074e34202af1f5175af28b8
dextraz <dextrazdxt@gmail.com>
2019-08-08 19:08:17 +0300

    Naive but be creative :-)

---
+++ SearchingAlgorithms/linear_search.js
@@ -0,0 +1,9 @@
+function linearSearch(arr, val){
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] === val) return i;
+    }
+    return -1;
+}
+
+console.log(linearSearch([34,51,1,2,3,45,56,687, 100], 100))
+console.log(linearSearch([34,51,1,2,3,45,56,687], 100))
