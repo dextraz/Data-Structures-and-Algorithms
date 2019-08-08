7c1481c14c2c0efaff81c4efbeb5061f1634a375
dextraz <dextrazdxt@gmail.com>
2019-08-08 17:50:11 +0300

    Queue

---
+++ Queue/queue.js
@@ -0,0 +1,15 @@
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
+
+module.exports = Queue;
